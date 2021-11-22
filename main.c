fafa5711bf8aa109c7c8e74cf630bd24593c2349
saa-lem <60645691+saa-lem@users.noreply.github.com>
2021-11-23 00:45:11 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,52 @@
+
+
+
+
+
+
+
+
+/* Users views users add delate login change password
+Catalogue
+Issuance
+Serial
+Acquisation
+*/
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <string.h>
+int menu();// prototype
+int main()
+{
+      int action;
+      action= menu (); //function call
+
+return 0;
+}
+//function definition
+int menu() //header
+{
+    int action;
+    do {
+        printf("County Library\n");
+        printf("Welcome Mr. Salem\n");
+        printf("What would you like to do?\n");
+        printf("1.View user.\n");
+        printf("2.Add users.\n");
+        printf("3.Edit user.\n");
+        printf("4.Delete user.");
+        printf("5.change password.\n");
+        printf("6.logout.\n");
+        printf("7.Exit.\n");
+        printf("Selected action(1-7):");
+        scanf("%d",&action);
+        if (action <1 ||action >7){
+              system ("cls");
+                printf("invalid Action.Try again");
+        }
+        }while (action < 1 || action > 7);
+
+
+return action;
+}
